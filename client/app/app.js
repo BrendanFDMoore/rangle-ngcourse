014edb3f42a57b0e44198406fb6793a79407334f
Brendan <git@bfdmoore.com>
2016-04-04 03:14:21 -0400

    Add $log injections. Add model usage on an input.

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('ngcourse', [])
-.controller('MainCtrl', function($scope) {
+.controller('MainCtrl', function($scope, $log) {
   $scope.username = 'alice';
   $scope.numberOfTasks = 0;
   $scope.addTask = function() {
+    $log.debug('Current number of tasks:', $scope.numberOfTasks);
     $scope.numberOfTasks += 1;
   };
 })
