f8caf46e960937d041073b3ea031e6a9bd849517
Brendan <git@bfdmoore.com>
2016-04-04 03:09:38 -0400

    Add simple controller and event handler in MainCtrl.

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('ngcourse', [])
+.controller('MainCtrl', function($scope) {
+  $scope.username = 'alice';
+  $scope.numberOfTasks = 0;
+  $scope.addTask = function() {
+    $scope.numberOfTasks += 1;
+  };
+})
 .run(function($log) {
   $log.info('All ready!');
 });
