760dc854d0102888dbab8107165b5131a7824f0d
Brendan <git@bfdmoore.com>
2016-04-05 22:36:42 -0400

    Change task list to use iterator across array. Push into array on add.

--- client/app/sections/task-list/task-list-controller.js
+++ client/app/sections/task-list/task-list-controller.js
@@ -6,10 +6,23 @@ angular.module('ngcourse')
 
   var vm = this;
 
-  vm.numberOfTasks = 0;
+  vm.tasks = [
+    {
+      owner: 'alice',
+      description: 'Build the dog shed.'
+    },
+    {
+      owner: 'bob',
+      description: 'Get the milk.'
+    },
+    {
+      owner: 'alice',
+      description: 'Fix the door handle.'
+    }
+  ];
 
   vm.addTask = function() {
-    vm.numberOfTasks += 1;
+    vm.tasks.push({owner:'alice',description:'Some new task...'})
   };
 })
 .run(function($log) {
