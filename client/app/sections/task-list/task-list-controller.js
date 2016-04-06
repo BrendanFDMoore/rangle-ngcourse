0110006257336de6531a01d779df6dcbbaa96eee
Brendan <git@bfdmoore.com>
2016-04-05 23:11:50 -0400

    Show promises broken apart.

--- client/app/sections/task-list/task-list-controller.js
+++ client/app/sections/task-list/task-list-controller.js
@@ -8,14 +8,21 @@ angular.module('ngcourse')
 
   vm.tasks = [];
 
-  $http.get('http://ngcourse.herokuapp.com/api/v1/tasks')
-    .then(function(response) {
-      $log.info(response);
-      vm.tasks = response.data;
-    })
-    .then(null, function(error) {
-      $log.error(error);
-    });
+  var responsePromise = $http.get('http://ngcourse.herokuapp.com/api/v1/tasks');
+  var tasksPromise = responsePromise.then(function(response) {
+    return response.data;
+  });
+  var filteredTasksPromise = tasksPromise.then(function(tasks) {
+    //return filterTasksAsynchronously(tasks);
+    return tasks;
+  });
+  var vmUpdatePromise = filteredTasksPromise.then(function(tasks) {
+    $log.info(tasks);
+    vm.tasks = tasks;
+  })
+  var errorHandlerPromise = vmUpdatePromise.then(null, function(error) {
+    $log.error(error);
+  });
 
   vm.addTask = function() {
     vm.tasks.push({owner:'alice',description:'Some new task...'})
