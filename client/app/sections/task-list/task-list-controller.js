b5cc65c1013df584ffd99a65569dd1f5d95bded1
Brendan <git@bfdmoore.com>
2016-04-05 23:16:42 -0400

    Switch to chained thens.

--- client/app/sections/task-list/task-list-controller.js
+++ client/app/sections/task-list/task-list-controller.js
@@ -8,21 +8,23 @@ angular.module('ngcourse')
 
   vm.tasks = [];
 
-  var responsePromise = $http.get('http://ngcourse.herokuapp.com/api/v1/tasks');
-  var tasksPromise = responsePromise.then(function(response) {
-    return response.data;
-  });
-  var filteredTasksPromise = tasksPromise.then(function(tasks) {
-    //return filterTasksAsynchronously(tasks);
-    return tasks;
-  });
-  var vmUpdatePromise = filteredTasksPromise.then(function(tasks) {
-    $log.info(tasks);
-    vm.tasks = tasks;
-  })
-  var errorHandlerPromise = vmUpdatePromise.then(null, function(error) {
-    $log.error(error);
-  });
+  vm.filterTasksAsynchronously = function(tks){
+    return tks;
+  };
+
+  $http.get('http://ngcourse.herokuapp.com/api/v1/tasks')
+    .then(function(response) {
+      return response.data;
+    })
+    .then(function(tasks) {
+      return vm.filterTasksAsynchronously(tasks);
+    })
+    .then(function(tasks) {
+      $log.info(tasks);
+      vm.tasks = tasks;
+    }, function(error) {
+      $log.error(error);
+    });
 
   vm.addTask = function() {
     vm.tasks.push({owner:'alice',description:'Some new task...'})
