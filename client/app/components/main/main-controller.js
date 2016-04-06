c9addf40ec93fc4fbd41bd5b4e6d6053cdb21abc
Brendan <git@bfdmoore.com>
2016-04-05 21:36:17 -0400

    Add $emit/$broadcast and $on example message passing

--- client/app/components/main/main-controller.js
+++ client/app/components/main/main-controller.js
@@ -2,7 +2,7 @@
 
 angular.module('ngcourse')
 
-.controller('MainCtrl', function($scope, $log) {
+.controller('MainCtrl', function($scope, $log, $window) {
   $scope.username = 'alice';  
   $scope.isAuthenticated = false;
 
@@ -10,6 +10,13 @@ angular.module('ngcourse')
     $scope.isAuthenticated = true;
   };
 
+  $scope.messageChild = function() {
+    $scope.$broadcast('hello.child', {fruit: 'peaches'});
+  };
+
+  $scope.$on('hello.parent', function(event, payload) {
+    $window.alert(payload.animal);
+  });
 })
 .run(function($log) {
   $log.info('MainCtrl ready!');
--- client/app/sections/task-list/task-list-controller.js
+++ client/app/sections/task-list/task-list-controller.js
@@ -2,12 +2,20 @@
 
 angular.module('ngcourse')
 
-.controller('TaskListCtrl', function($scope, $log) {
+.controller('TaskListCtrl', function($scope, $log, $window) {
   $scope.numberOfTasks = 0;
   
   $scope.addTask = function() {
     $scope.numberOfTasks += 1;
   };
+
+  $scope.messageParent = function() {
+    $scope.$emit('hello.parent', {animal: 'turtle'})
+  };
+
+  $scope.$on('hello.child', function(event, payload) {
+    $window.alert(payload.fruit);
+  });
 })
 .run(function($log) {
   $log.info('TaskListCtrl ready!');
